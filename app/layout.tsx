9738a0707c3675f02e6d705290d83ff458d95306
yelin <sky3nlq@gmail.com>
2024-08-13 11:47:29 +0900

    [FIX] web title 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "World Congress PCE",
+  title: "World Congress O-PCE",
   description: "World Congress Patient Counseling Web Page",
   icons: {
     icon: "/favicon.ico",
